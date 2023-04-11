c85a20bfd302faabc38fa9e5bfe8c21b2e6de985
SvetLanaPesh <124920308+SvetLanaPesh@users.noreply.github.com>
2023-04-11 15:06:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,53 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T *arr;
+    int first;
+    int last;
+    int count;
+ public:
+    TPQueue() : first(0), last(0), count(0) { arr = new T[size]; }
+    bool isEmpty() const;
+    bool isFull() const;
+    void push(const T&);
+    const T& pop();
 };
-
+template <typename T, int size>
+bool TPQueue<T, size>::isEmpty() const {
+	return 0 == count;
+}
+template <typename T, int size>
+bool TPQueue<T, size>::isFull() const {
+    return size == count;
+}
+template <typename T, int size>
+void TPQueue<T, size>::push(const T& value) {
+    if (isFull()) {
+        throw std::string("Full!");
+    } else {
+        ++count;
+        arr[last % size] = value;
+        for (int last1 = last; last1 > first && arr[last1 % size].prior > arr[(last1-1) % size].prior; last1--) {
+            T temp = arr[last1 % size];
+            arr[last1 % size] = arr[(last1 - 1) % size];
+            arr[(last1 - 1) % size] = temp;
+        }
+        last++;
+    }
+}
+template <typename T, int size>
+const T& TPQueue<T, size>::pop() {
+    if (isEmpty()) {
+        throw std::string("Empty!");
+    } else {
+        --count;
+        return arr[first++ % size];
+    }
+}
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
